3106cc8ed3dcfd2af03dd61f3deb42734843d633
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-07 16:01:16 +0200

    add greyscale filter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-const imgCarouselEl = document.getElementById("img-carousel");
+const carouselEl = document.getElementById("img-carousel");
 const beforeButtonEl = document.querySelector(".fa-chevron-left");
 const afterButtonEl = document.querySelector(".fa-chevron-right");
-const imagesEl = document.getElementById("images");
+const secondaryImgContainer = document.getElementById("images");
 
 const photos = [
    {path: "./assets/img/1.jpg"},
@@ -15,25 +15,32 @@ const photos = [
 let i = 0; // i is the position in the array
 const imgEl = document.createElement("img");
 imgEl.src = `${photos[i].path}`; // add src dynamically
-imgCarouselEl.append(imgEl);
+carouselEl.append(imgEl);
 
 // Secondary imgs
-photos.forEach((photo, i) => {
-   const secImgEl = document.createElement("img");
-   secImgEl.src = `${photos[i].path}`; // add src dynamically
-   secImgEl.style.width = `calc(100% / ${photos.length})`;
-   imagesEl.append(secImgEl);
-})
+photos.forEach((photo, index) => {
+   const secondaryImgEl = document.createElement("img");
+   secondaryImgEl.src = `${photos[index].path}`; // add src dynamically
+   secondaryImgEl.style.width = `calc(100% / ${photos.length})`;   
+   (index == i ? "" : secondaryImgEl.classList.toggle("inactive")); // set inactive for all the photos expect the first
+   secondaryImgContainer.append(secondaryImgEl);
+});
+
+const secondaryImgs = document.querySelectorAll("#images > img");
 
 beforeButtonEl.addEventListener("click", function(){
+   secondaryImgs[i].classList.toggle("inactive");
    i--;
    imgEl.src = `${photos[i < 0 ?  i = photos.length - 1 : i].path}`;
    // Ternary operator to have a loop (skip to first img to last one)
-})
+   secondaryImgs[i].classList.toggle("inactive")
+});
 
 afterButtonEl.addEventListener("click", function(){
+   secondaryImgs[i].classList.toggle("inactive");
    i++;   
    imgEl.src = `${photos[i > photos.length - 1 ? i = 0 : i].path}`;
    // Ternary operator to have a loop (skip to last img to first one)
-})
+   secondaryImgs[i].classList.toggle("inactive")
+});
 
