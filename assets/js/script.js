d8cce0fb28a5f0fdfd4e8dbeeff67c71cade55ab
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-07 15:11:38 +0200

    add secondary imgs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const imgCarouselEl = document.getElementById("img-carousel");
 const beforeButtonEl = document.querySelector(".fa-chevron-left");
 const afterButtonEl = document.querySelector(".fa-chevron-right");
+const imagesEl = document.getElementById("images");
 
 const photos = [
    {path: "./assets/img/1.jpg"},
@@ -10,24 +11,29 @@ const photos = [
    {path: "./assets/img/5.jpg"},
 ]
 
-let i = 0;
+// Main img
+let i = 0; // i is the position in the array
 const imgEl = document.createElement("img");
-imgEl.src = `${photos[i].path}`;
+imgEl.src = `${photos[i].path}`; // add src dynamically
 imgCarouselEl.append(imgEl);
 
+// Secondary imgs
+photos.forEach((photo, i) => {
+   const secImgEl = document.createElement("img");
+   secImgEl.src = `${photos[i].path}`; // add src dynamically
+   secImgEl.style.width = `calc(100% / ${photos.length})`;
+   imagesEl.append(secImgEl);
+})
+
 beforeButtonEl.addEventListener("click", function(){
-   i--;   
-   if (i < 0) {
-      i = photos.length - 1;
-   }
-   imgEl.src = `${photos[i].path}`;
+   i--;
+   imgEl.src = `${photos[i < 0 ?  i = photos.length - 1 : i].path}`;
+   // Ternary operator to have a loop (skip to first img to last one)
 })
 
 afterButtonEl.addEventListener("click", function(){
    i++;   
-   if (i > photos.length -1) {
-      i = 0;
-   }
-   imgEl.src = `${photos[i].path}`;
+   imgEl.src = `${photos[i > photos.length - 1 ? i = 0 : i].path}`;
+   // Ternary operator to have a loop (skip to last img to first one)
 })
 
